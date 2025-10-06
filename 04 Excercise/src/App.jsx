083fc13a96cfb4b9f86b8b07983b91acdd639eb7
dreadlord-sedai <dahamifabbio@gmail.com>
2025-10-06 09:33:20 +0530

    04 excercise

---
+++ 04 Excercise/src/App.jsx
@@ -0,0 +1,13 @@
+import ConceptCards from './components/ConceptCards';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <ConceptCards />
+    </div>
+  );
+}
+
+export default App;
