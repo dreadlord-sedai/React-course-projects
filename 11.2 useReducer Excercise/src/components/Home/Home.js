b02c75095d1f42c0477df977524011afb9712204
dreadlord-sedai <dahamifabbio@gmail.com>
2025-10-11 15:13:23 +0530

    11.2 Excercise

--- 11.2 useReducer Excercise/src/components/Home/Home.js
+++ 11.2 useReducer Excercise/src/components/Home/Home.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Home.module.css';
+import AuthContext from '../../store/auth-context';
 
 const Home = (props) => {
   return (
