b0ce40a842542a5eb98108270f4d7644214536fd
dreadlord-sedai <dahamifabbio@gmail.com>
2025-10-16 20:39:22 +0530

    12 Project Max Schwazmuller's course is all over the place and i cant understand anything

--- 12 ReactMeals Project/src/components/Layout/HeaderCartButton.js
+++ 12 ReactMeals Project/src/components/Layout/HeaderCartButton.js
@@ -1,19 +1,41 @@
-import {useContext} from "react";
+import { useState, useContext, useEffect } from "react";
 
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
 import classes from "./HeaderCartButton.module.css";
 
 function HeaderCartButton(props) {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+
   const cartCtx = useContext(CartContext);
 
-  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+  const { items } = cartCtx;
+
+  const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${
+    numberOfCartItems ? classes.bump : ""
+  }`;
 
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+    
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
